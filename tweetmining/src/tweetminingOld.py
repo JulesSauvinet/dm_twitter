c2550115e4877ad862a9f0000638bfa49bb0b291
Marine RUIZ <Marine RUIZ>
2017-01-19 15:08:47 +0100

    On filtre avec la blacklist

--- tweetmining/src/tweetminingOld.py
+++ tweetmining/src/tweetminingOld.py
@@ -5,19 +5,16 @@ from datetime import timedelta, datetime
 
 # on appelle MongoDBHandler pour tout ce qui concerne la connexion a la base de donnees
 # on appelle OptimisedEventDetectorMEDBased pour ce qui concerne la construction de la matrice de similarite et la construction des clusters
-from scipy import stats
-from statsmodels.stats import gof
 
 from eventDetectionFromTwitter.source.controller.DataManagement.MongoDBHandler import MongoDBHandler
 from eventDetectionFromTwitter.source.controller.EventDetection.OptimisedEventDetectorMEDBased import \
     OptimisedEventDetectorMEDBased
-from testStat import filterTweets
 
 #MIN_TERM_OCCURENCE_E -> pourcentage d'apparition d'un terme en fonction du nombre de tweet d'un cluster
 #MIN_TERM_OCCURENCE -> nombre d'occurence minimal d'un terme
 #ELASTICITY -> booleen pour savoir si on utilise MIN_TERM_OCCURENCE_E ou MIN_TERM_OCCURENCE
-#REMOVE_NOISE_WITH_POISSON_LAW -> booleen pour savoir si on supprime les termes qui sont regis par une loi de Poisson
-#GEOLOCALISATION -> booleen pour savoir pour si on fait des clusters de densite avant de faire des clusters de similarite
+#REMOVE_NOISE_WITH_POISSON_LAW -> booleen pour savoir si on supprime les termes qui sont régis par une loi de Poisson
+#GEOLOCALISATION -> booleen pour savoir pour si on fait des clusters de densité avant de faire des clusters de similarite
 
 MIN_TERM_OCCURENCE_E=0.2
 MIN_TERM_OCCURENCE=20
@@ -44,6 +41,8 @@ def getTweetsFromCSVRepositoryAndSave(repositoryPath="..\\data\\smallTweets3_fil
     mongoDBHandler=MongoDBHandler()
     mongoDBHandler.saveTweetsFromCSVRepository(repositoryPath)
 
+#firstdate = "2015-07-21"
+#firstdate = "2015-09-16"
 #---------------------------------------------------------------------------------------------------------------------------------------------
 def main(limit=3000, minimalTermPerTweet=MIN_TERM_OCCURENCE,
 		minimalTermPerTweetElasticity=MIN_TERM_OCCURENCE_E,
@@ -68,12 +67,7 @@ def main(limit=3000, minimalTermPerTweet=MIN_TERM_OCCURENCE,
             maxTime = tweet.time
     timeTotal = maxTime-minTime
 
-    totalEvent = []
-    blackList = []
-	
-    # --------------------------------------- on fait un premier clustering ------------------------------------- #
-	
-    # blackList = {} on ne peut pas la remplir au debut
+    # blackList = {} on ne peut pas la remplir au d?but
     # pour toutes les dates de nos donnees dans MongoDB
         # on lance un premier clustering
     # on stocke dans un tableau tous les hashtags pertinents
@@ -92,14 +86,12 @@ def main(limit=3000, minimalTermPerTweet=MIN_TERM_OCCURENCE,
 
         staringTime = time.time()
         tweets = mongoDBHandler.getAllTweetsOfDate(limit=limit,date=datestring)
-        print "tweet before filter : ", len(tweets)
-        #tweets = filterTweets(tweets)
-        print "tweet after filter : ", len(tweets)
 
         if (tweets):
             if (len(tweets)>0) :
 
                 print "date : ", datestring
+                sortieFile.write("date : " + datestring)
                 eventDetector = OptimisedEventDetectorMEDBased(tweets, timeResolution=TIME_RESOLUTION,
                                                                distanceResolution=DISTANCE_RESOLUTION, scaleNumber=SCALE_NUMBER,
                                                                minSimilarity=MIN_SIMILARITY, distanceThreshold=DISTANCE_THRESHOLD)
@@ -107,60 +99,7 @@ def main(limit=3000, minimalTermPerTweet=MIN_TERM_OCCURENCE,
                 events = eventDetector.getEvents(datestring, minimalTermPerTweet=minimalTermPerTweet,
                                                  minimalTermPerTweetElasticity=minimalTermPerTweetElasticity,
                                                  remove_noise_with_poisson_Law=remove_noise_with_poisson_Law,
-                                                 elasticity=elasticity, geolocalisation=geolocalisation,blackList=blackList)
-
-                if len(events) > 0:
-                    totalEvent.extend(events)
-
-    # ----------------------------------------- fin du premier clustering --------------------------------------- #
-	
-    #avec SmallTweet trie on detecte 110 evenements    
-    numberOfEvent = float(len(totalEvent))
-    pertinentHashtag = {}
-    numberOfHash = 0
-
-    # on enregistre tous les hashtags avec leur nbr d'occurence
-    for event in totalEvent :
-        hashtagsEvent = event.importantHashtags
-        for hashs in hashtagsEvent :
-            try:
-                 pertinentHashtag[hashs] += 1.0
-            except KeyError:
-                pertinentHashtag[hashs] = 1.0
-                numberOfHash += 1
-
-    # on calcule la frequence d'apparition
-    for hashs,occurence in pertinentHashtag.iteritems() :
-        freq = occurence/numberOfEvent
-        if freq > 0.25 :
-            blackList.append(hashs)
-
-    # ---------------------------------------- on fait un second clustering ------------------------------------- #
-    for i in range(timeTotal.days+1):
-        mongoDBHandler = MongoDBHandler()
-        end_date = minTime + timedelta(days=i)
-
-        datestring = end_date.strftime('%Y-%m-%d')
-
-        staringTime = time.time()
-        tweets = mongoDBHandler.getAllTweetsOfDate(limit=limit,date=datestring)
-        print "tweet before filter : ", len(tweets)
-        #tweets = filterTweets(tweets)
-        print "tweet after filter : ", len(tweets)
-        if (tweets):
-            if (len(tweets)>0) :
-
-                print "date : ", datestring
-                sortieFile.write("date : " + datestring)
-                sortieFile.write
-                eventDetector = OptimisedEventDetectorMEDBased(tweets, timeResolution=TIME_RESOLUTION,
-                                                            distanceResolution=DISTANCE_RESOLUTION, scaleNumber=SCALE_NUMBER,
-                                                            minSimilarity=MIN_SIMILARITY, distanceThreshold=DISTANCE_THRESHOLD)
-
-                events = eventDetector.getEvents(datestring, minimalTermPerTweet=minimalTermPerTweet,
-                                                 minimalTermPerTweetElasticity=minimalTermPerTweetElasticity,
-                                                 remove_noise_with_poisson_Law=remove_noise_with_poisson_Law,
-                                                 elasticity=elasticity, geolocalisation=geolocalisation, blackList=blackList)
+                                                 elasticity=elasticity, geolocalisation=geolocalisation)
 
                 if len(events) > 0:
                     print("")
@@ -176,9 +115,10 @@ def main(limit=3000, minimalTermPerTweet=MIN_TERM_OCCURENCE,
                         print(event)
                         print("*" * 80)
                         sortieFile.write(event.__str__()+"\n")
+
                         vizuFile.write(event.outForVizu()+"\n")
+
                         sortieFile.write("********************************************************************************\n")
-                    
                     elapsed_time=(time.time()-staringTime)
                     print("-"*40)
                     print("Elapsed time : {0}s".format(elapsed_time))
@@ -202,12 +142,10 @@ def main(limit=3000, minimalTermPerTweet=MIN_TERM_OCCURENCE,
                     sortieFile.write("----------------------------------------\n")
                     sortieFile.write("Elapsed time : {0}s".format(elapsed_time)+"\n")
                     sortieFile.write("----------------------------------------\n\n\n")
+                    
 
-    for hashs in blackList :
-        print hashs 
     vizuFile.close()
-    sortieFile.close()    
-
+    sortieFile.close()
     for f in glob.glob("output*.txt"):
         os.remove(f)
     for f in glob.glob("input*.txt"):
